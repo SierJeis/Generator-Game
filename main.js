0c4587c0bcf0f53f35d10e09dad308540ad239fc
Jeis <sirjeis15@gmail.com>
2022-08-24 20:43:53 -0400

    yes

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ var gen3cost = 1000;
 var gen4cost = 10000;
 var tickspeed = 1000;
 var tickspeedcost = 100;
+var gen1multi = 1;
+var gen2multi = 1;
+var gen3multi = 1;
+var gen4multi = 1;
 var myInterval = setInterval(mytimer, tickspeed);
 const formatter = new Intl.NumberFormat('en', {
     notation: 'compact'
@@ -25,11 +29,11 @@ function clicked(){
 function updatecount(){
     setInterval(() => {
         document.querySelector("#clicks").innerHTML = "Clicks: "+formatter.format(clicks);
-        document.querySelector("#gen1").innerHTML = "Tier 1 Generator: "+formatter.format(gen1)+" {"+formatter.format(gen1bought)+"} <br> Cost: "+formatter.format(gen1cost);+" Clicks"
-        document.querySelector("#gen2").innerHTML = "Tier 2 Generator: "+formatter.format(gen2)+" {"+formatter.format(gen2bought)+"} <br> Cost: "+formatter.format(gen2cost);+" Clicks"
-        document.querySelector("#gen3").innerHTML = "Tier 3 Generator: "+formatter.format(gen3)+" {"+formatter.format(gen3bought)+"} <br> Cost: "+formatter.format(gen3cost);+" Clicks"
-        document.querySelector("#gen4").innerHTML = "Tier 4 Generator: "+formatter.format(gen4)+" {"+formatter.format(gen4bought)+"} <br> Cost: "+formatter.format(gen4cost);+" Clicks"
-        document.querySelector("#cps").innerHTML = "Clicks Per Tick: "+formatter.format((gen1+gen1bought));
+        document.querySelector("#gen1").innerHTML = "Tier 1 Generator: "+formatter.format(gen1)+" {"+formatter.format(gen1bought)+"} ×"+gen1multi+"<br> Cost: "+formatter.format(gen1cost);+" Clicks";
+        document.querySelector("#gen2").innerHTML = "Tier 2 Generator: "+formatter.format(gen2)+" {"+formatter.format(gen2bought)+"} ×"+gen2multi+"<br> Cost: "+formatter.format(gen2cost);+" Clicks";
+        document.querySelector("#gen3").innerHTML = "Tier 3 Generator: "+formatter.format(gen3)+" {"+formatter.format(gen3bought)+"} ×"+gen3multi+"<br> Cost: "+formatter.format(gen3cost);+" Clicks";
+        document.querySelector("#gen4").innerHTML = "Tier 4 Generator: "+formatter.format(gen4)+" {"+formatter.format(gen4bought)+"} ×"+gen4multi+"<br> Cost: "+formatter.format(gen4cost);+" Clicks";
+        document.querySelector("#cps").innerHTML = "Clicks Per Tick: "+formatter.format((gen1*gen1multi));
         document.querySelector("#tickspeed").innerHTML = "Upgrade Tickspeed <br> Cost: "+formatter.format(tickspeedcost)+"<br> Tickspeed: "+Math.round(tickspeed);
     }, 10);
 }
@@ -47,38 +51,58 @@ function upgrade(){
 function buygen(){
     if(clicks >= gen1cost){
         gen1bought += 1;
+        gen1 += 1;
         clicks -= gen1cost;
-        gen1cost *= 1.2;
+    }
+    if(gen1bought >= 10){
+        gen1bought = 0;
+        gen1multi *= 2;
+        gen1cost *= 10;
     }
 }
 
 function buygen2(){
     if(clicks >= gen2cost){
         gen2bought += 1;
+        gen2 += 1;
         clicks -= gen2cost;
-        gen2cost *= 1.5;
+    }
+    if(gen2bought >= 10){
+        gen2bought = 0;
+        gen2multi *= 2;
+        gen2cost *= 10;
     }
 }
 
 function buygen3(){
     if(clicks >= gen3cost){
         gen3bought += 1;
+        gen3 += 1;
         clicks -= gen3cost;
-        gen3cost *= 2.3;
+    }
+    if(gen3bought >= 10){
+        gen3bought = 0;
+        gen3multi *= 2;
+        gen3cost *= 10;
     }
 }
 
 function buygen4(){
     if(clicks >= gen4cost){
         gen4bought += 1;
+        gen4 += 1;
         clicks -= gen4cost;
-        gen4cost *= 2.9;
+    }
+    if(gen4bought >= 10){
+        gen4bought = 0;
+        gen4multi *= 2;
+        gen4cost *= 10;
     }
 }
 
 function mytimer(){
-    clicks += gen1bought+gen1;
-    gen1 += gen2+gen2bought;
-    gen2 += gen3+gen3bought;
-    gen3 += gen4+gen4bought;
+    clicks += (gen1*gen1multi);
+    gen1 += (gen2*gen2multi);
+    gen2 += (gen3*gen3multi);
+    gen3 += (gen4*gen4multi);
 }
